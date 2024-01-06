4e10a39fd046369428e2ae89dc7a420817b46732
Akhila113-xxx <155696347+Akhila113-xxx@users.noreply.github.com>
2024-01-06 17:12:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,15 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 
+##########################Let's Get All the Rows, Not Just One
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 
 
