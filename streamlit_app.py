c39946c4af92dcd33ce28f1c689294afa5036078
Akhila113-xxx <155696347+Akhila113-xxx@users.noreply.github.com>
2024-01-06 19:24:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,23 +30,12 @@ fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 #output it as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 
 # New section to display fruity vice api responce
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
-
-####################output is not as expected##############################
-
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
 
 
 ################################### Let's Query Some Data, Instead
